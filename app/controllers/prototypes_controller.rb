cb2066ff170a29f1419c8b0e8c5759a14ef7e973
yuyakiso <yuya2226@gmail.com>
2020-12-18 18:51:32 +0900

    check again commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :new, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @prototypes = Prototype.includes(:user)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
   def show
     @user = User.find(params[:id])
+    @prototype = @user.prototypes
     @prototypes = Prototype.includes(:user)
   end
 end
